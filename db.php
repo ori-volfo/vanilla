f7bd26133cc4df3e6b0214dd52610ca35a24cc11
ori-volfo <ori.volfo@gmail.com>
2018-01-21 17:59:05 +0200

    DB => PHP lower than 7

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 
     CONST HOST = 'localhost';
-    CONST DB = 'sisense';
+    CONST DB = '';
     CONST USERNAME = 'root';
     CONST PASSWORD = 1;
 
@@ -11,12 +11,19 @@
         {
             echo "Failed to connect to MySQL: " . mysqli_connect_error();
         }
+
+//        PHP 5
+//        $db = mysql_connect(HOST, USERNAME, PASSWORD);
+//        mysql_select_db(DB, $db);
+
 		return $db; // Resource
 	}
 	
 	function query($query){
 		$db = connect();
 		$resource = mysqli_query($db, $query);
+//		  PHP 5
+//        $resource = mysql_query($query, $db);
 		$res = array();
 		if (stripos($query, 'select') !== FALSE)
 		{
